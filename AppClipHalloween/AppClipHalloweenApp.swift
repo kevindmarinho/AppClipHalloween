d8988355cf733499d6c665c5bf5c42b779f36021
kevin marinho <kevindmarinho4321@gmail.com>
2022-10-10 15:55:08 -0400

    Initial Commit

---
+++ AppClipHalloween/AppClipHalloweenApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppClipHalloweenApp.swift
+//  AppClipHalloween
+//
+//  Created by kevin marinho on 10/10/22.
+//
+
+import SwiftUI
+
+@main
+struct AppClipHalloweenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
